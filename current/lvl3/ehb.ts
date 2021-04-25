42873b4d5894612e2c89976c4dcf581405a0853f
dekvall <dkvldev@gmail.com>
2021-04-26 01:45:54 +0200

    rate update

--- current/lvl3/ehb.ts
+++ current/lvl3/ehb.ts
@@ -1,7 +1,7 @@
 export default
 {
   "type": "ehb",
-  "date": "2021-01-06",
+  "date": "2021-04-26",
   "bosses": [
     {
       "boss": "abyssal_sire",
--- current/main/misc.ts
+++ current/main/misc.ts
@@ -1,7 +1,7 @@
 export default
 {
   "type": "misc",
-  "date": "2021-01-06",
+  "date": "2021-04-26",
   "bosses": [
     {
       "boss": "clue_all",
