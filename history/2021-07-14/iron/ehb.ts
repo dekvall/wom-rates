49e49eddffcd5f8afc22da21f684511d22b7879d
dekvall <dkvldev@gmail.com>
2021-07-14 23:34:51 +0200

    correct iron ehb

--- history/2021-07-14/iron/ehb.ts
+++ history/2021-07-14/iron/ehb.ts
@@ -5,99 +5,127 @@ export default
   "bosses": [
     {
       "boss": "abyssal_sire",
-      "rate": 42.0
+      "rate": 32.0
     },
     {
       "boss": "alchemical_hydra",
-      "rate": 27.0
+      "rate": 26.0
+    },
+    {
+      "boss": "barrows_chests",
+      "rate": 18.0
+    },
+    {
+      "boss": "bryophyta",
+      "rate": 10.0
     },
     {
       "boss": "callisto",
-      "rate": 50.0
+      "rate": 30.0
     },
     {
       "boss": "cerberus",
-      "rate": 61.0
+      "rate": 54.0
     },
     {
       "boss": "chambers_of_xeric",
-      "rate": 3.0
+      "rate": 2.6
     },
     {
       "boss": "chambers_of_xeric_challenge_mode",
-      "rate": 2.2
+      "rate": 1.65
     },
     {
       "boss": "chaos_elemental",
-      "rate": 60.0
+      "rate": 48.0
     },
     {
       "boss": "chaos_fanatic",
-      "rate": 100.0
-    },
-    {
-      "boss": "commander_zilyana",
-      "rate": 55.0
-    },
-    {
-      "boss": "corporeal_beast",
-      "rate": 50.0
-    },
-    {
-      "boss": "dagannoth_prime",
-      "rate": 88.0
-    },
-    {
-      "boss": "dagannoth_rex",
-      "rate": 88.0
-    },
-    {
-      "boss": "dagannoth_supreme",
-      "rate": 88.0
-    },
-    {
-      "boss": "general_graardor",
-      "rate": 50.0
-    },
-    {
-      "boss": "giant_mole",
-      "rate": 100.0
-    },
-    {
-      "boss": "grotesque_guardians",
-      "rate": 36.0
-    },
-    {
-      "boss": "kalphite_queen",
-      "rate": 50.0
-    },
-    {
-      "boss": "king_black_dragon",
-      "rate": 120.0
-    },
-    {
-      "boss": "kraken",
-      "rate": 90.0
-    },
-    {
-      "boss": "kreearra",
-      "rate": 25.0
-    },
-    {
-      "boss": "kril_tsutsaroth",
-      "rate": 65.0
-    },
-    {
-      "boss": "sarachnis",
       "rate": 80.0
     },
     {
+      "boss": "commander_zilyana",
+      "rate": 34.0
+    },
+    {
+      "boss": "corporeal_beast",
+      "rate": 10.0
+    },
+    {
+      "boss": "crazy_archaeologist",
+      "rate": 75.0
+    },
+    {
+      "boss": "dagannoth_prime",
+      "rate": 105.0
+    },
+    {
+      "boss": "dagannoth_rex",
+      "rate": 105.0
+    },
+    {
+      "boss": "dagannoth_supreme",
+      "rate": 105.0
+    },
+    {
+      "boss": "deranged_archaeologist",
+      "rate": 80.0
+    },
+    {
+      "boss": "general_graardor",
+      "rate": 25.0
+    },
+    {
+      "boss": "giant_mole",
+      "rate": 80.0
+    },
+    {
+      "boss": "grotesque_guardians",
+      "rate": 32.0
+    },
+    {
+      "boss": "hespori",
+      "rate": 46.0
+    },
+    {
+      "boss": "kalphite_queen",
+      "rate": 30.0
+    },
+    {
+      "boss": "king_black_dragon",
+      "rate": 75.0
+    },
+    {
+      "boss": "kraken",
+      "rate": 76.0
+    },
+    {
+      "boss": "kreearra",
+      "rate": 21.0
+    },
+    {
+      "boss": "kril_tsutsaroth",
+      "rate": 25.0
+    },
+    {
+      "boss": "mimic",
+      "rate": 60.0
+    },
+    {
+      "boss": "obor",
+      "rate": 12.0
+    },
+    {
+      "boss": "sarachnis",
+      "rate": 56.0
+    },
+    {
       "boss": "scorpia",
-      "rate": 130.0
+      "rate": 60.0
     },
     {
       "boss": "skotizo",
-      "rate": 45.0
+      "rate": 38.0
     },
     {
       "boss": "the_gauntlet",
@@ -105,19 +133,19 @@ export default
     },
     {
       "boss": "the_corrupted_gauntlet",
-      "rate": 6.5
+      "rate": 7.0
     },
     {
       "boss": "theatre_of_blood",
-      "rate": 3.0
+      "rate": 2.4
     },
     {
       "boss": "thermonuclear_smoke_devil",
-      "rate": 125.0
+      "rate": 95.0
     },
     {
       "boss": "tzkal-zuk",
-      "rate": 0.8
+      "rate": 0.85
     },
     {
       "boss": "tztok-jad",
@@ -125,11 +153,11 @@ export default
     },
     {
       "boss": "venenatis",
-      "rate": 50.0
+      "rate": 35.0
     },
     {
       "boss": "vetion",
-      "rate": 30.0
+      "rate": 23.0
     },
     {
       "boss": "vorkath",
@@ -137,15 +165,11 @@ export default
     },
     {
       "boss": "zulrah",
-      "rate": 35.0
+      "rate": 32.0
     },
     {
       "boss": "the_nightmare",
-      "rate": 14.0
-    },
-    {
-      "boss": "theatre_of_blood_challenge_mode",
-      "rate": 3.0
+      "rate": 11.0
     },
     {
       "boss": "phosanis_nightmare",
